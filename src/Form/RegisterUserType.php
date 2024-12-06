3c2f741017fa76b130b5be20d2ce83450065e9fb
Kelaven <kevin.lavenant@live.fr>
2024-12-06 18:25:58 +0100

    feat(RegisterUserType): form upgraded

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class RegisterUserType extends AbstractType
@@ -14,11 +17,33 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('firstname')
-        ->add('lastname')
-        ->add('email')
-        ->add('password')
-        ->add('submit', SubmitType::class)
+        ->add('firstname', TextType::class, [
+            'label' => "Votre prénom",
+                        'attr' => [
+                'placeholder' => "John",
+            ]
+        ])
+        ->add('lastname', TextType::class, [
+            'label' => "Votre nom",
+                        'attr' => [
+                'placeholder' => "Doe"
+            ]
+        ])
+        ->add('email', EmailType::class, [
+            'label' => "Votre adresse email",
+            'attr' => [
+                'placeholder' => "john@doe.fr"
+            ]
+        ])
+        ->add('password', PasswordType::class, [
+            'label' => "Votre mot de passe",
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => "Valider",
+            'attr' => [
+                'class' => "btn btn-success"
+            ]
+        ])
         ;
     }
 
