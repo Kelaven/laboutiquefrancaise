b48d6b63ed2d0172522e570588fefbe2d3473f91
Kelaven <kevin.lavenant@live.fr>
2024-12-07 12:04:08 +0100

    feat(RegisterUserType): form finished

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -6,16 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\RegisterUserType;
-
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
 
-        $form = $this->createForm(RegisterUserType::class);
+        $user = new User(); // on crée un nouvel utilisateur pour l'envoyer plus tard en BDD
 
+        $form = $this->createForm(RegisterUserType::class, $user); // j'ajoute $user pour remplir le nouvel utilisateur crée juste avant, qui est alors encore vide
+
+        $form->handleRequest($request); // écouter ce que l'utilisateur soumet dans le form
+
+        if ($form->isSubmitted() && $form->isValid()) {
+// dd($form->getData());
+            $entityManager->persist($user); // figer les données. On lui indique qu'on veut enregistrer les données dans la table user. 
+            $entityManager->flush(); // enregistrer les données
+
+        }
 
         return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView()
--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -10,6 +10,10 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 
 
 class RegisterUserType extends AbstractType
@@ -19,13 +23,25 @@ class RegisterUserType extends AbstractType
         $builder
         ->add('firstname', TextType::class, [
             'label' => "Votre prénom",
-                        'attr' => [
+            'constraints' => [
+                new Length([
+                    'min' => 2,
+                    'max' => 30
+                    ])
+            ],
+            'attr' => [
                 'placeholder' => "John",
             ]
         ])
         ->add('lastname', TextType::class, [
             'label' => "Votre nom",
-                        'attr' => [
+            'constraints' => [
+                new Length([
+                    'min' => 2,
+                    'max' => 30
+                    ])
+            ],
+            'attr' => [
                 'placeholder' => "Doe"
             ]
         ])
@@ -35,8 +51,28 @@ class RegisterUserType extends AbstractType
                 'placeholder' => "john@doe.fr"
             ]
         ])
-        ->add('password', PasswordType::class, [
-            'label' => "Votre mot de passe",
+        ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'constraints' => [
+                new Length([
+                    'min' => 4,
+                    'max' => 30
+                    ])
+            ],
+            'first_options'  => [
+                'label' => 'Votre mot de passe', 
+                'hash_property_path' => 'password', // hacher
+                'attr' => [
+                    'placeholder' => "123456"
+                ],
+            ],
+            'second_options' => [
+                'label' => 'Confirmer Votre mot de passe',
+                'attr' => [
+                    'placeholder' => "123456"
+                ]
+            ],
+            'mapped' => false, // ne pas faire le lien avec l'entity User car dedans le champ s'appelle password et non pas plainPassword
         ])
         ->add('submit', SubmitType::class, [
             'label' => "Valider",
@@ -50,6 +86,12 @@ class RegisterUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'constraints' => [ // Ajouter la contrainte de mail unique dans le form si on ne souhaite pas le faire dans l'entity
+                new UniqueEntity([
+                    'entityClass' => User::class,
+                    'fields' => 'email'
+                ])
+                ],
             'data_class' => User::class,
         ]);
     }
